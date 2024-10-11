8ecd191ca538e0506280d87577f24e0c2f8f02f2
Alexandre Nunes <123529883+xandenunes42@users.noreply.github.com>
2024-10-11 14:38:22 -0300

    Criando arquivos
    Subindo exemplo de utilizacao de criacao e leitura de arquivos

---
+++ Excecoes&Colecoes/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] leitor = File.ReadAllLines("Arquivos/arquivo.txt");
+
+foreach (string leitorLine in leitor)
+{
+    Console.WriteLine(leitorLine);
+}
